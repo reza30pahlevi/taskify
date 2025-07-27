c5ba262484e85df0ce4d1182137eb61a4ccdd6cd
RezaPahlevi1 <flashwatchreza@gmail.com>
2025-07-27 20:57:47 +0700

    koneksi ke supabase

---
+++ src/supabaseClient.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://izenpygdrexegwbjnypo.supabase.co";
+const supabseAnonkey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6ZW5weWdkcmV4ZWd3YmpueXBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2MjI2MDAsImV4cCI6MjA2OTE5ODYwMH0.GJPsMWPBwSkAR0ofmnhWLSIEjf3oD-TFs6gW3r8QRaQ";
+
+export const supabase = createClient(supabaseUrl, supabseAnonkey);
